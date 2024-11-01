23f3e537af29bd7769238c5c6f6988ebd7667bec
BIAOUU <20030125yb@gmail.com>
2024-11-01 20:48:42 +0800

    Initial commit

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 
 const createToken = (_id) => {
